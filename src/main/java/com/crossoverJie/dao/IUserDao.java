358be3ab464bb5b93ee51bb21859763c976c5beb
crossoverJie <731756881@qq.com>
2016-06-22 00:12:07 +0800

    提交dao

---
+++ src/main/java/com/crossoverJie/dao/IUserDao.java
@@ -0,0 +1,31 @@
+package com.crossoverJie.dao;
+
+import com.crossoverJie.pojo.User;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+public interface IUserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    User findByLogin(User user);
+
+    List<User> findByParams(User u, RowBounds rowBound);
+
+    int findAllCount(User u);
+
+    List<User> findHotUser();
+}
