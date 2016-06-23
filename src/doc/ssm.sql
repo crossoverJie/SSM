e7ed7e862049219a67053434a0382496572701e5
crossoverJie <731756881@qq.com>
2016-06-23 15:16:46 +0800

    sql

---
+++ src/doc/ssm.sql
@@ -0,0 +1,31 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50629
+Source Host           : localhost:3306
+Source Database       : ssm
+
+Target Server Type    : MYSQL
+Target Server Version : 50629
+File Encoding         : 65001
+
+Date: 2016-06-23 15:11:28
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
