748ba85503d8d16ec9cebe933381743f43d8a88f
crossoverJie <731756881@qq.com>
2016-07-28 17:53:20 +0800

    Server

--- src/main/java/com/crossoverJie/cxf/impl/Server.java
+++ src/main/java/com/crossoverJie/cxf/impl/Server.java
@@ -6,7 +6,7 @@ import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 public class Server {
 
 	public static void main(String[] args) {
-		System.out.println("web service start");
+		System.out.println("web service 启动中。。。");
 		HelloWorld implementor= new HelloWorldImpl();
 		/**修改为自己电脑的IP**/
 		String address="http://192.168.1.19:9000/helloWorld";
@@ -15,6 +15,6 @@ public class Server {
 		factoryBean.setServiceClass(HelloWorld.class); // 接口类
 		factoryBean.setServiceBean(implementor); // 设置实现类
 		factoryBean.create(); // 创建webservice接口
-		System.out.println("web service started");
+		System.out.println("web service 启动成功。。");
 	}
 }
