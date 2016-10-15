fc82bb076d5b8b99334a04f230a1075e74618b25
crossoverJie <731756881@qq.com>
2016-10-15 19:06:24 +0800

    age

--- src/main/java/com/crossoverJie/pojo/User.java
+++ src/main/java/com/crossoverJie/pojo/User.java
@@ -11,6 +11,7 @@ public class User {
     private String username ;
     private String password ;
     private String description ;
+    private int age ;
 
 
 
@@ -44,5 +45,13 @@ public class User {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
