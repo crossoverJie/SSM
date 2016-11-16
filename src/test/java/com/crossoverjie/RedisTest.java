25a27723062951210ee58b253239ae5d0a662ad5
crossoverJie <crossoverjie@gmail.com>
2016-11-17 01:38:54 +0800

    Redis初始化

--- src/test/java/com/crossoverjie/RedisTest.java
+++ src/test/java/com/crossoverjie/RedisTest.java
@@ -1,19 +1,11 @@
 package com.crossoverjie;
 
 
-import com.crossoverJie.service.ContentService;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/*.xml"})
 public class RedisTest {
-
-    @Autowired
-    private ContentService contentService;
-
-
-
 }
