a76881916478d0f2d8a3919529ecc7e9e6e35188
crossoverJie <crossoverjie@gmail.com>
2016-11-24 00:23:29 +0800

    方法拦截器

--- src/test/java/com/crossoverjie/RedisTest.java
+++ src/test/java/com/crossoverjie/RedisTest.java
@@ -7,8 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.List;
@@ -30,16 +28,17 @@ public class RedisTest {
         config.setMaxWaitMillis(1000);
 
 
-        JedisPool pool = new JedisPool(config, "27.126.180.210", 6379);
-        System.out.println(pool.getResource());
-        Jedis jedis = pool.getResource();
-        jedis.set("name", "陈杰");
-        System.out.println(jedis.get("name"));
+//        JedisPool pool = new JedisPool(config, "27.126.180.210", 6379);
+//        System.out.println(pool.getResource());
+//        Jedis jedis = pool.getResource();
+//        jedis.set("name", "陈杰");
+//        System.out.println(jedis.get("name"));
 
     }
 
     @Test
     public void testCache() {
+        System.out.println("---------------");
         List<Content> contentList = contentService.findContentList();
         List<Content> contentList1 = contentService.findContentList();
         System.out.println(contentList == contentList1);
