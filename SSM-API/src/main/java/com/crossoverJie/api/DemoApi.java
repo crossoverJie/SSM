51a9d9ba4c6bbf8cd6fd91bb0697cf7fc0b9a29f
crossoverJie <crossoverjie@gmail.com>
2017-03-01 23:44:44 +0800

    API

---
+++ SSM-API/src/main/java/com/crossoverJie/api/DemoApi.java
@@ -0,0 +1,17 @@
+package com.crossoverJie.api;
+
+/**
+ * Function:
+ * @author chenjiec
+ * Date: 2017/3/1 下午11:42
+ * @since JDK 1.7
+ */
+public interface DemoApi {
+    /**
+     * 获取用户信息
+     * @param userId
+     * @return
+     * @throws RuntimeException
+     */
+    public String readMsg(int userId) throws RuntimeException ;
+}
