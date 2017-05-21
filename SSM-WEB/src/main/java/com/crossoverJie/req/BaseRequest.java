402b94b87f9eeacb3fd02f05bd3425e74e838f80
crossoverJie <crossoverjie@gmail.com>
2017-05-21 23:52:12 +0800

    请求基类

---
+++ SSM-WEB/src/main/java/com/crossoverJie/req/BaseRequest.java
@@ -0,0 +1,38 @@
+package com.crossoverJie.req;
+
+import java.io.Serializable;
+
+public abstract class BaseRequest implements Serializable {
+
+    private static final long serialVersionUID = 268113588289950599L;
+    /**
+     * 请求号
+     */
+    private String reqNo;
+
+    /**
+     * 当前请求的时间戳
+     */
+    private int timeStamp;
+
+ 
+    public BaseRequest() {
+        this.setTimeStamp((int)(System.currentTimeMillis() / 1000));
+    }
+
+    public String getReqNo() {
+        return reqNo;
+    }
+
+    public void setReqNo(String reqNo) {
+        this.reqNo = reqNo;
+    }
+
+    public int getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(int timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
