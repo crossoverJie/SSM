7a6d62e4432f1d981454f78e56273bdb2ea8b583
crossoverJie <crossoverjie@gmail.com>
2018-04-18 01:32:36 +0800

    :white_check_mark: concurrent test

---
+++ SSM-SECONDS-KILL/SSM-SECONDS-KILL-WEB/src/test/com/crossoverjie/CurrentTest.java
@@ -0,0 +1,64 @@
+package com.crossoverjie;
+
+import com.crossoverJie.seconds.kill.controller.IndexController;
+import com.crossoverJie.seconds.kill.util.HttpTools;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+
+public class CurrentTest {
+
+    private static Logger logger = LoggerFactory.getLogger(CurrentTest.class);
+    private static ExecutorService executorServicePool;
+    private static String url="http://47.98.194.60:8083/SSM-SECONDS-KILL-WEB-2.2.0-SNAPSHOT/getStockNum" ;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        init();
+        for (int i = 0; i < 250; i++) {
+
+            executorServicePool.execute(new Worker(i));
+        }
+
+        executorServicePool.shutdown() ;
+        while (!executorServicePool.awaitTermination(1,TimeUnit.SECONDS)){
+            logger.info("worker running");
+        }
+        logger.info("worker over");
+    }
+
+    public static void init() {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("current-thread-%d").build();
+        executorServicePool = new ThreadPoolExecutor(150, 150, 0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(200), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+    }
+
+
+
+
+    private static class Worker implements Runnable{
+
+        private int index ;
+
+        public Worker(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public void run() {
+
+            String res = HttpTools.doGet(url, HttpClients.createDefault()) ;
+            logger.info("=======index{}====res{}====",index,res);
+        }
+    }
+
+
+}
