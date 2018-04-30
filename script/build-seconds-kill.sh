72c589e97236ba036e3156d1a35984606c906568
crossoverJie <crossoverjie@gmail.com>
2018-05-01 00:33:04 +0800

    :sparkles: Introducing new features.新增自动化脚本

---
+++ script/build-seconds-kill.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+read appname
+
+echo "input="$appname
+
+PID=$(ps -e|grep $appname|awk '{printf $1}')
+
+if [ $? -eq 0 ]; then
+    echo "process id:$PID"
+else
+    echo "process $appname not exit"
+    exit
+fi
+
+
+kill -9 ${PID}
+
+if [ $? -eq 0 ];then
+    echo "kill $appname success"
+else
+    echo "kill $appname fail"
+fi
+
+git pull
+
+cd SSM-SECONDS-KILL
+
+mvn -Dmaven.test.skip=true clean package
+
+echo "build war success"
+
+cp /home/crossoverJie/SSM/SSM-SECONDS-KILL/SSM-SECONDS-KILL-SERVICE/target/SSM-SECONDS-KILL-SERVICE-2.2.0-SNAPSHOT.war /home/crossoverJie/tomcat/tomcat-dubbo-provider-8080/webapps
+
+echo "cp war ok!"
+
+
+
+sh /home/crossoverJie/tomcat/tomcat-dubbo-provider-8080/bin/startup.sh
+
+echo "start $appname success"
