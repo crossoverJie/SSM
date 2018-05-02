f08029dec2df37b87856c8bc56d822bb82db9d85
crossoverJie <crossoverjie@gmail.com>
2018-05-03 00:29:43 +0800

    :bento: 添加或更新资源 优化脚本 web

--- script/build-seconds-kill-web.sh
+++ script/build-seconds-kill-web.sh
@@ -2,29 +2,24 @@
 
 # 构建 web 消费者
 
-read appname
+#read appname
 
+appname="consumer"
 echo "input="$appname
 
 
 PID=$(ps -ef | grep $appname | grep -v grep | awk '{print $2}')
 
-if [ $? -eq 0 ]; then
-    echo "process id:$PID"
-else
-    echo "process $appname not exit"
-    exit
-fi
 
-echo "pid = $PID"
-kill -9 ${PID}
+# 遍历杀掉 pid
+for var in ${PID[@]};
+do
+    echo "loop pid= $var"
+    kill -9 $var
+done
 
-if [ $? -eq 0 ];then
-    echo "kill $appname success"
-else
-    echo "kill $appname fail"
-    exit
-fi
+echo "kill $appname success"
+
 
 cd ..
 
@@ -37,13 +32,19 @@ mvn -Dmaven.test.skip=true clean package
 echo "build war success"
 
 cp /home/crossoverJie/SSM/SSM-SECONDS-KILL/SSM-SECONDS-KILL-WEB/target/SSM-SECONDS-KILL-WEB-2.2.0-SNAPSHOT.war /home/crossoverJie/tomcat/tomcat-dubbo-consumer-8083/webapps
+echo "cp tomcat-dubbo-consumer-8083/webapps ok!"
 
-echo "cp war ok!"
+cp /home/crossoverJie/SSM/SSM-SECONDS-KILL/SSM-SECONDS-KILL-WEB/target/SSM-SECONDS-KILL-WEB-2.2.0-SNAPSHOT.war /home/crossoverJie/tomcat/tomcat-dubbo-consumer-7083-slave/webapps
+echo "cp tomcat-dubbo-consumer-7083-slave/webapps ok!"
 
 
 
 
 
 sh /home/crossoverJie/tomcat/tomcat-dubbo-consumer-8083/bin/startup.sh
+echo "tomcat-dubbo-consumer-8083/bin/startup.sh success"
+
+sh /home/crossoverJie/tomcat/tomcat-dubbo-consumer-7083-slave/bin/startup.sh
+echo "tomcat-dubbo-consumer-7083-slave/bin/startup.sh success"
 
 echo "start $appname success"
