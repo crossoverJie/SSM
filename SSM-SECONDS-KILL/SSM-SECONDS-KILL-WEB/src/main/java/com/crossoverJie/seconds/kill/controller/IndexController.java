292e646cd695a04e1b1fd287e1a2cc78df3cd4b8
crossoverJie <crossoverjie@gmail.com>
2018-05-06 18:09:20 +0800

    :sparkles: Introducing new features.errorCode

--- SSM-SECONDS-KILL/SSM-SECONDS-KILL-WEB/src/main/java/com/crossoverJie/seconds/kill/controller/IndexController.java
+++ SSM-SECONDS-KILL/SSM-SECONDS-KILL-WEB/src/main/java/com/crossoverJie/seconds/kill/controller/IndexController.java
@@ -107,7 +107,7 @@ public class IndexController {
      * @param sid
      * @return
      */
-    @SpringControllerLimit(errorCode = 200)
+    @SpringControllerLimit
     @RequestMapping("/createOptimisticLimitOrderByRedis/{sid}")
     @ResponseBody
     public String createOptimisticLimitOrderByRedis(@PathVariable int sid) {
