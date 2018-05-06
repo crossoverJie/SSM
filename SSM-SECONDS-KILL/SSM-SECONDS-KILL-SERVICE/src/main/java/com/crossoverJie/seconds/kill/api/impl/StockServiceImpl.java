8b6484d15725bc3407fdc8739ade2a80d58d4d28
crossoverJie <crossoverjie@gmail.com>
2018-05-06 15:23:55 +0800

    :bug: Fixing a bug. 空指针

--- SSM-SECONDS-KILL/SSM-SECONDS-KILL-SERVICE/src/main/java/com/crossoverJie/seconds/kill/api/impl/StockServiceImpl.java
+++ SSM-SECONDS-KILL/SSM-SECONDS-KILL-SERVICE/src/main/java/com/crossoverJie/seconds/kill/api/impl/StockServiceImpl.java
@@ -34,8 +34,9 @@ public class StockServiceImpl implements StockService {
         String remoteAddressString = RpcContext.getContext().getRemoteHostName();
         logger.info("request ={}",remoteAddressString);
 
-        int count = (int) redisTemplate.opsForValue().get("sid_1");
-        if (count == 0){
+        Object oCount = redisTemplate.opsForValue().get("sid_1");
+        int count =0 ;
+        if (oCount == null){
             Stock stock = stockService.getStockById(1);
             count = stock.getCount() ;
             redisTemplate.opsForValue().set("sid_1",count) ;
