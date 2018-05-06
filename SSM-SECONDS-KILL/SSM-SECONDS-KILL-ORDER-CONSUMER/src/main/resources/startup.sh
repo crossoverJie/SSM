c3dac9259ba4f61cb52048c0d568d6c8b8178728
crossoverJie <crossoverjie@gmail.com>
2018-05-06 23:22:07 +0800

    :construction_worker: 添加 CI 构建到系统

--- SSM-SECONDS-KILL/SSM-SECONDS-KILL-ORDER-CONSUMER/src/main/resources/startup.sh
+++ SSM-SECONDS-KILL/SSM-SECONDS-KILL-ORDER-CONSUMER/src/main/resources/startup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kill -9 `cat ${jar_path}/application.pid`
+kill -9 `cat application.pid`
 
 nohup java -jar bigdata-1.0.0-SNAPSHOT.jar -Xmx128m -Xms128m --logging.config=/data/schedule/kafka/bigdata/config/logback.xml --logging.file=/dev/null  > /dev/null 2>&1 &
